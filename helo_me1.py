ea1782e4951e64732f7940eeaad48cf9a70a3852
mooracle <yanuar.prakosa@gmail.com>
2021-02-12 15:50:15 +0700

    use python in VSCode trial

---
+++ helo_me1.py
@@ -0,0 +1,8 @@
+import fizzbuzz as fb
+
+print("Hello world")
+print("This is the first heloo world using conda and VSCode")
+print("I need to remember using ctrl+shift+P combination in VSCode")
+
+#try to use fizzbuzz method; note that I already imported the fizzbuzz as fb
+fb.fizzbuzz(3, 5, 100)
