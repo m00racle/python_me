358c69f904c7e5315c39d235a2f2132a4d56538d
mooracle <yanuar.prakosa@gmail.com>
2021-06-05 13:58:17 +0700

    debug docdist8, update docs and diagrams

--- 6-006_python/lec02_code/docdist8.py
+++ 6-006_python/lec02_code/docdist8.py
@@ -130,6 +130,14 @@ def vector_angle(D1,D2):
 def main():
     if len(sys.argv) != 3:
         print ("Usage: docdist8.py filename_1 filename_2")
+        #-- FOR DEBUG PURPOSES ONLY
+        filename_1 = 'E:\\python_me\\6-006_python\\lec02_code\\t1.verne.txt'
+        filename_2 = 'E:\\python_me\\6-006_python\\lec02_code\\t2.bobsey.txt'
+        sorted_word_list_1 = word_frequencies_for_file(filename_1)
+        sorted_word_list_2 = word_frequencies_for_file(filename_2)
+        distance = vector_angle(sorted_word_list_1,sorted_word_list_2)
+        print ("The distance between the documents is: %0.6f (radians)"%distance)
+        # --- comment out this after finish debugging!
     else:
         filename_1 = sys.argv[1]
         filename_2 = sys.argv[2]
