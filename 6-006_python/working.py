5d7ce25dc5dfd460726dd736402131687ce270d8
mooracle <yanuar.prakosa@gmail.com>
2021-08-28 14:54:21 +0700

    cleaning front files

--- 6-006_python/working.py
+++
@@ -1,7 +0,0 @@
-import os
-
-currentWorkingDir = os.getcwd()
-print(currentWorkingDir)
-
-trialFile = open('./6-006_python/trial.txt')
-print(trialFile.read())
--- helo_me1.py
+++
@@ -1,8 +0,0 @@
-import fizzbuzz as fb
-
-print("Hello world")
-print("This is the first heloo world using conda and VSCode")
-print("I need to remember using ctrl+shift+P combination in VSCode")
-
-#try to use fizzbuzz method; note that I already imported the fizzbuzz as fb
-fb.fizzbuzz(5, 8, 10)
