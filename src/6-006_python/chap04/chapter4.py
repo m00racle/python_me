b49247a4f07450819778af1daca65a8250d225d9
mooracle <yanuar.prakosa@gmail.com>
2021-10-01 08:05:17 +0700

    max subarray using divide and conqueror tested

--- src/6-006_python/chap04/chapter4.py
+++ src/6-006_python/chap04/chapter4.py
@@ -13,15 +13,16 @@ def find_max_cross_subarray(A, low, mid, high) -> tuple :
 
     Parameter:
     A : array like of number
-    low : first component index in the array A (int)
-    mid : middle component index in the array A (int)
-    high: last component index in the array A (int)
+    low : first component index in the array A starting from 1 (int) 
+    mid : middle component index in the array A starting from 1 (int)
+    high: last component index in the array A starting from 1 (int)
     maxVal: (default True) true means seek for maximum sum false means minimum sum (boolean)
     """
     left_sum = -math.inf
     sum = 0
-    for i in range(mid-1, 0, -1):
-        sum = sum + A[i]
+    for i in range(mid, 0, -1):
+        # BUG RESOLVED: the lower limit is set to 0 instead of 1 since it is mid <= i < 0 thus 0 will be omitted!
+        sum = sum + A[idx(i)]
         if sum > left_sum:
             left_sum = sum
             max_left = i
@@ -29,11 +30,36 @@ def find_max_cross_subarray(A, low, mid, high) -> tuple :
     # for the right side
     right_sum = -math.inf
     sum = 0
-    for j in range(mid, high):
-        sum = sum + A[j]
+    for j in range(mid + 1, high+1):
+        sum = sum + A[idx(j)]
         if sum > right_sum:
             right_sum = sum
             max_right = j
     
     return (max_left, max_right, left_sum + right_sum)
-    
+
+def find_max_subarray(A, low, high):
+    """
+    find max subarray of delta values.
+    return: tuple(low (int), high (int), sum (int))
+
+    parameters: 
+    A : array like of numbers (List)
+    low : index of lowest position in array to be inspected (int)
+    high : index of highest position in array to be inspected (int)
+    """
+    if high == low:
+        return (low, high, A[idx(low)])
+    else:
+        mid = (low + high) // 2
+        # the recursion is not merging but COMBINATION: the low and high parameters passed is constant!
+        (left_low, left_high, left_sum) = find_max_subarray(A, low, mid)
+        (right_low, right_high, right_sum) = find_max_subarray(A, mid + 1, high)
+        (cross_low, cross_high, cross_sum) = find_max_cross_subarray(A, low, mid, high)
+        # choose which to return
+        if left_sum >= right_sum and left_sum >= cross_sum :
+            return (left_low, left_high, left_sum)
+        elif right_sum >= left_sum and right_sum >= cross_sum:
+            return (right_low, right_high, right_sum)
+        else: return (cross_low, cross_high, cross_sum)
+
--- test/test_chapter4.py
+++ test/test_chapter4.py
@@ -4,7 +4,7 @@ tested_path = os.path.normpath(os.path.dirname(__file__) + "/../src/6-006_python
 sys.path.append(tested_path)
 
 import unittest
-from chapter4 import find_max_cross_subarray as cross
+from chapter4 import find_max_cross_subarray as cross, find_max_subarray as subs
 
 class TestChap04(unittest.TestCase):
     """
@@ -24,9 +24,16 @@ class TestChap04(unittest.TestCase):
         # prepare
         crossA = [-1, -3, 12, -6, 10, -3, 7, 3, -12, 8, -1]
         # assert
-        self.assertEqual(cross(crossA, 0, len(crossA)//2, len(crossA)), (2, 7, 23))
+        self.assertEqual(cross(crossA, 0, len(crossA)//2, len(crossA)), (3, 8, 23))
         
-    
+    def test_find_max_subarray(self):
+        """
+        test finding maximum sub-array
+        """
+        # prepare
+        testA = [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7]
+        # assert (7, 10, 43)
+        self.assertEqual(subs(testA, 1, len(testA)), (8, 11, 43))
     
 if __name__ == "__main__":
     unittest.main(verbosity=2)
